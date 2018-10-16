f1d914f6110cf19b2b643f627b8a7030f443c772
Nathan Wreggit <wreggit@cs.washington.edu>
2018-10-16 16:31:27 -0700

    Organize and MVP level design done

---
+++ CoolNamePending/Assets/Scripts/CarDrive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDrive : MonoBehaviour {
+
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += transform.forward * -speed;
+        }
+    }
+}
