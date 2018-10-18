83415a7a6e044ddcc5d1df829a21822b3df8ccda
Nathan Wreggit <wreggit@cs.washington.edu>
2018-10-18 16:30:29 -0700

    Police car assets and initial failure script

---
+++ CoolNamePending/Assets/Scripts/CarTreeCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTreeCollision : MonoBehaviour {
+
+    public GameObject explosionParent;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        print(rb.velocity.magnitude);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.relativeVelocity.magnitude > 5)
+        {
+            print("Ded");
+            explosionParent.SetActive(true);
+        }
+    }
+}
+
+
