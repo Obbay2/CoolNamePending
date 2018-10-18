45cc4fed446849c64166494f643ef235216e21b5
Nathan Wreggit <obbay2@hotmail.com>
2018-10-17 17:54:35 -0700

    Working vehicle

--- CoolNamePending/Assets/Scripts/CarDrive.cs
+++ CoolNamePending/Assets/Scripts/CarDrive.cs
@@ -1,31 +1,60 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class CarDrive : MonoBehaviour {
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
 
-    public float speed;
-    private Rigidbody rb;
+public class CarDrive : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
-
-    private void FixedUpdate()
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
     {
-        if (Input.GetKey(KeyCode.W))
+        if (collider.transform.childCount == 0)
         {
-            rb.AddForce(transform.forward * speed, ForceMode.VelocityChange);
-        } 
-        else if (Input.GetKey(KeyCode.S))
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
         {
-            rb.AddForce(transform.forward * -speed, ForceMode.VelocityChange);
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
     }
 }
