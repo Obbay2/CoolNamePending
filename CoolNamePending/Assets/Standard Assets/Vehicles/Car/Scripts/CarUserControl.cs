f1f21f9dba1ff7663fd0c11ede13bac366c2b7ad
Nathan Wreggit <wreggit@cs.washington.edu>
2018-10-25 16:41:37 -0700

    Animated steering wheel and steering wheel control enabled by default

--- CoolNamePending/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ CoolNamePending/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -8,7 +8,10 @@ namespace UnityStandardAssets.Vehicles.Car
     public class CarUserControl : MonoBehaviour
     {
         public bool Controller;
+        public bool SteeringWheel;
+        public Transform SteeringWheelMesh;
         private CarController m_Car; // the car controller we want to use
+        private static int SteerMultiplier = 450;
 
 
         private void Awake()
@@ -21,21 +24,31 @@ namespace UnityStandardAssets.Vehicles.Car
         private void FixedUpdate()
         {
             // pass the input to the car!
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
-
             if (Controller)
             {
-                v = Input.GetAxis("Right Trigger") - Input.GetAxis("Left Trigger");
-                h = Input.GetAxis("Left Joystick");
+                float v = Input.GetAxis("Right Trigger") - Input.GetAxis("Left Trigger");
+                float h = Input.GetAxis("Left Joystick");
+                m_Car.Move(h, v, v, 0);
+            }
+            else if (SteeringWheel)
+            {
+                float footbrake = Input.GetAxis("Footbrake"); // -1 to 1 => 0 to 1
+                footbrake = (footbrake + 1) / 2;
+                float h = Input.GetAxis("SteeringWheel"); // -1 to 1
+                float v = Input.GetAxis("Accelerator"); // -1 to 1 => 0 to 1
+                v = (v + 1) / 2;
+                SteeringWheelMesh.eulerAngles = new Vector3(SteeringWheelMesh.eulerAngles.x, SteeringWheelMesh.eulerAngles.y, h * SteerMultiplier);
+                print(v + " " + h + " " + footbrake);
+                m_Car.Move(h, v, footbrake, 0);
+            }
+            else
+            {
+                float h = CrossPlatformInputManager.GetAxis("Horizontal");
+                float v = CrossPlatformInputManager.GetAxis("Vertical");
+                m_Car.Move(h, v, v, 0);
             }
             
-#if !MOBILE_INPUT
-            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-            m_Car.Move(h, v, v, handbrake);
-#else
-            m_Car.Move(h, v, v, 0f);
-#endif
+            
         }
     }
 }
