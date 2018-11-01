77b12470583cf2d9cea49ef7e1e9d72b5e05081a
Student User <deanbarlan@gmail.com>
2018-10-31 21:38:27 -0700

    Added car spawning script

---
+++ CoolNamePending/Assets/Scripts/TrafficSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficSpawner : MonoBehaviour {
+
+    public GameObject car;          // The car prefab to be spawned.
+    public float spawnTime = 3f;    // How long between each spawn.
+    public Transform[] spawnPoints;    // The spawn point the car can spawn from.
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+		 * If we decide to have multiple spawn points then we just uncomment the code below
+		 * int spawnPointIdx = Random.Range(0, spawnPoints.Length);
+		 * 
+		 */
+
+        int spawnPointIdx = 0;
+
+        Instantiate(car, spawnPoints[spawnPointIdx].position, spawnPoints[spawnPointIdx].rotation);
+	}
+}
