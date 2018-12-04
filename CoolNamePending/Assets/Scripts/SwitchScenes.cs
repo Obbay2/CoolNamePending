a6b47cd569fe519a282ff7c5571bc82bda2e3835
Student User <studentuser@D-10-19-3-123.dhcp4.washington.edu>
2018-12-03 16:22:51 -0800

    Finished switching back to main

--- CoolNamePending/Assets/Scripts/SwitchScenes.cs
+++ CoolNamePending/Assets/Scripts/SwitchScenes.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SwitchScenes : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void Start()
+    {
+        StartCoroutine(SwitchToMain());
+    }
+
+    // Update is called once per frame
+    void Update () {
         // return to main menu if F1 or a button on steering wheel is pressed
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.F1))
+        /*if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.F1))
         {
             SceneManager.LoadScene("MainScene");
-        }
+        }*/
 	}
-    
-    void SwitchToMain()
+
+    IEnumerator SwitchToMain()
     {
-        SceneManager.LoadSceneAsync("MainScene");
+        yield return null;
+        AsyncOperation asyncOp = SceneManager.LoadSceneAsync("MainScene");
+        asyncOp.allowSceneActivation = false;
+        while (!asyncOp.allowSceneActivation)
+        {
+            if (asyncOp.progress >= 0.9f)
+            {
+                //Wait to you press the space key to activate the Scene
+                if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.F1))
+                    //Activate the Scene
+                    asyncOp.allowSceneActivation = true;
+            }
+            yield return null;
+        }
     }
 }
